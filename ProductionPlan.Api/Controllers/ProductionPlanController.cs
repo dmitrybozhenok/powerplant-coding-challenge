2650a25406a29c45bb4a5c0fd92ae79d086a4753
Dmitry Bozhenok <dbozhenok@systemorph.com>
2023-10-01 17:33:55 +0100

    Added controller

---
+++ ProductionPlan.Api/Controllers/ProductionPlanController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using ProductionPlan.Api.Model;
+using ProductionPlan.Api.Services;
+
+namespace ProductionPlan.Api.Controllers
+{
+    [ApiController]
+    public class ProductionPlanController : ControllerBase
+    {
+        private readonly ILogger<ProductionPlanController> logger;
+        private readonly IProductionPlanService productionPlanService;
+
+
+        public ProductionPlanController(ILogger<ProductionPlanController> logger, IProductionPlanService productionPlanService)
+        {
+            this.logger = logger;
+            this.productionPlanService = productionPlanService;
+        }
+
+        [HttpPost("productionplan")]
+        public IReadOnlyCollection<ProductionPlanItem> GetProductionPlan([FromBody] ProductionPlanRequest request)
+        {
+            logger.LogInformation("Production plan request received");
+            return productionPlanService.GetProductionPlan(request);
+        }
+    }
+}
